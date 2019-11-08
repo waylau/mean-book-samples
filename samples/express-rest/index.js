bbb6acba25c76d97b9096b5709c3b84716117ce1
waylau <waylau521@gmail.com>
2019-11-08 21:01:54 +0800

    修改了注释

--- samples/express-rest/index.js
+++ samples/express-rest/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-const bodyParser = require('body-parser');//用于req.body获取值的
+const bodyParser = require('body-parser');//用于从req.body中获取值
 app.use(bodyParser.json());
 
 
